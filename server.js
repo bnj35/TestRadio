10cfcf76d21ac512036a9c7d897153f3cd66155c
bnj35 <benjamin.auger6@etu.univ-lorraine.fr>
2024-11-07 16:31:12 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const http = require('http');
+const WebSocket = require('ws');
+const fs = require('fs');
+
+// Create HTTP server to serve HTML files
+const server = http.createServer((req, res) => {
+  let filePath = './broadcaster.html';
+  if (req.url === '/listener') {
+    filePath = './listener.html';
+  }
+  
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end("File not found");
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    }
+  });
+});
+
+// Create WebSocket server and attach it to HTTP server
+const wss = new WebSocket.Server({ server });
+
+let broadcaster = null;
+const listeners = new Set();
+
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    try {
+      const data = JSON.parse(message);
+
+      if (data.type === 'broadcast') {
+        broadcaster = ws;
+        ws.isBroadcaster = true;
+        console.log("Broadcaster connected");
+      } else if (data.type === 'listen') {
+        listeners.add(ws);
+        console.log("Listener connected. Total listeners:", listeners.size);
+      } else if (data.type === 'audio') {
+        if (ws.isBroadcaster) {
+          // Forward audio data to all listeners
+        //   console.log("Forwarding audio to listeners");
+          listeners.forEach(listener => {
+            if (listener.readyState === WebSocket.OPEN) {
+              listener.send(JSON.stringify({ audio: data.audio }));
+            }
+          });
+        }
+      }
+    } catch (error) {
+      console.error("Error parsing message:", error);
+    }
+  });
+
+  ws.on('close', (code, reason) => {
+    if (ws.isBroadcaster) {
+      broadcaster = null;
+      console.log("Broadcaster disconnected");
+    } else {
+      listeners.delete(ws);
+      console.log("Listener disconnected. Remaining listeners:", listeners.size);
+    }
+    console.log("WebSocket closed with code:", code, "Reason:", reason);
+  });
+});
+
+server.listen(8080, () => {
+  console.log("HTTP and WebSocket server running on http://localhost:8080");
+});
